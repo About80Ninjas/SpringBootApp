fafba11eb0dd3ffec9a8584d07fa3dc69fdeac32
About80Ninjas <kd0shk@gmail.com>
2018-10-19 01:06:05 -0500

    Added buid webhook

--- src/main/java/us/about80ninjas/app/controller/IndexController.java
+++ src/main/java/us/about80ninjas/app/controller/IndexController.java
@@ -1,5 +1,11 @@
 package us.about80ninjas.app.controller;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -7,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @Controller
@@ -21,6 +28,29 @@ public class IndexController {
 		model.addAttribute("ip", ip);
 		return "index";
 	}
+	
+	@GetMapping(value="/job/SpringBootApp/build")
+	public String buildWebHook() {
+		URL url;
+		try {
+			url = new URL("http://localhost:8080/job/SpringBootApp/build?TOKEN=mPj6hYZ5YeDy3C58AHzpz6nsr");
+			HttpURLConnection con;
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.disconnect();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "redirect:index";
+	}
+	
 	
 	
 
