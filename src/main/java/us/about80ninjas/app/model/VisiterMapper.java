66b6be9bec695f1e25fcb1910e628b4a2c81d252
About80Ninjas <jordan.henry@outlook.com>
2018-10-22 20:30:31 -0500

    Added border

--- src/main/java/us/about80ninjas/app/model/VisiterMapper.java
+++ src/main/java/us/about80ninjas/app/model/VisiterMapper.java
@@ -1,5 +1,7 @@
 package us.about80ninjas.app.model;
 
+import java.sql.Timestamp;
+
 import org.springframework.web.client.RestTemplate;
 
 public class VisiterMapper {
@@ -7,6 +9,7 @@ public class VisiterMapper {
 	public static final Visiter getIpInfo(String ip) {
 		RestTemplate restTemplate = new RestTemplate();
 		Visiter visiter = restTemplate.getForObject("http://ipinfo.io/" + ip + "?TOKEN=0ab37b5ad6eefc", Visiter.class);
+		visiter.setTimestamp(new Timestamp(System.currentTimeMillis()));
 		return visiter;
 	}
 
